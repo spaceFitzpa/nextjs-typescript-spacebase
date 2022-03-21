1c856e2c8cfff2f707607dcdb7743273f2bddc7f
Louie Fitzpatrick <louie.fitzpatrick@space150.com>
2022-03-21 14:18:50 -0500

    initial next app with typescript and spacebase

---
+++ .prettierrc.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  enable: true,
+  bracketSameLine: true,
+  printWidth: 120,
+  trailingComma: "none",
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false
+}
